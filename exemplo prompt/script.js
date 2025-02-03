7d63f8d5578d71e7f98a5f1d0687ec06b3eb561a
Isabela Paiola <isabelapaiola6@gmail.com>
2025-02-03 16:24:34 -0300

    Add files via upload

---
+++ exemplo prompt/script.js
@@ -0,0 +1,7 @@
+let nome = prompt("Digite o seu nome")
+let idade = prompt ("Digite sua idade")
+
+
+
+// document, procura ID (), adiciona o texto (innerText) e adiciona a variável*/
+document.getElementById("resultado").innerText = " Olá " + nome + ", você tem " + idade + " anos de idade."
---
+++ exemplo variavel/script.js
@@ -0,0 +1,7 @@
+let nome = ("Isabela")
+let bairro = ("Vila curuça")
+console.log(nome)
+alert (nome)
+
+// document, procura ID (), adiciona o texto (innerText) e adiciona a variável*/
+document.getElementById("resultado").innerText = " Olá " + nome + ", você mora no bairro" + bairro + ", boa tarde"
